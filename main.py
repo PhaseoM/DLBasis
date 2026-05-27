d3c6e4240bc3ac5db4dd95396cf3f17d3efcbf3c
phaseom <2929137524@qq.com>
2026-05-27 22:22:31 +0800

    rm track lab2/models

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     # lab2.conf.block_n = 3
     # lab2.resnet.main()
     # lab2.plaincnn.main()
-    lab2.conf.block_n = 5
-    lab2.resnet.main(sample_index=42)
-    # lab2.plaincnn.main()
-    # lab2.conf.block_n = 9
+    # lab2.conf.block_n = 5
     # lab2.resnet.main()
+    # lab2.plaincnn.main()
+    lab2.conf.block_n = 9
+    lab2.resnet.main()
     # lab2.plaincnn.main()
     # lab2.conf.block_n = 18
     # lab2.resnet_110.main()
